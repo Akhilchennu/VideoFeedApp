d844b776d330f7fc0620e3f643ff862643cf7d4f
Akhil Chennu <akhilchennu@Akhils-MacBook-Pro.local>
2020-10-10 23:16:36 +0530

    Initial setup

---
+++ src/components/videoDisplay.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+
+function VideoDisplay(props){
+
+    const [startx, setStartx] = useState(0);
+
+    const onTouchStarting = (event) => {
+        setStartx(event.touches[0].clientx);
+    }
+
+    const onTouchMoving = (event) => {
+        let touch = event.touches[0];
+        let change = touch.clientx - startx;
+        let baseTarget = document.getElementById(`${event.target.id}`);
+        let target = document.getElementById(`user${event.target.id.substring(5)}`)
+        if (change < 0) {
+            return
+        }
+        baseTarget.style.left = '-' + change + 'px';
+        target.style.display = 'block';
+        target.style.left = ((window.screen.width) - change) + 'px';
+    }
+
+    const onTouchEnding = (event) => {
+        let change = startx - event.changedTouches[0].clientx;
+        let threshold = window.screen.width / 3;
+        let baseTarget = document.getElementById(`${event.target.id}`);
+        let target = document.getElementById(`user${event.target.id.substring(5)}`)
+        if (change < threshold) {
+            baseTarget.style.left = 0;
+            target.style.left = '100%';
+            target.style.display = 'none';
+        } else {
+            baseTarget.style.transition = "all .3s";
+            target.style.transition = "all .3s";
+            baseTarget.style.display = '-100%';
+            target.style.left = '0';
+            target.style.display = 'flex';
+            baseTarget.style.display = 'none'
+            target.style.height = `${window.screen.height}px`;
+            target.style.width = `${window.screen.width}px`;
+        }
+    }
+
+    const onUserTouchStarting = (event) => {
+        setStartx(event.touches[0].clientx);
+        let baseTarget = document.getElementById(`video${event.target.id.substring(4)}`);
+        let target = document.getElementById(`${event.target.id}`);
+        baseTarget.style.transition = '';
+        target.style.transition = '';
+        baseTarget.style.display = 'none'
+    }
+
+    const onUserTouchMoving = (event) => {
+        let touch = event.touches[0];
+        let change = touch.clientx - startx;
+        let baseTarget = document.getElementById(`video${event.target.id.substring(4)}`);
+        let target = document.getElementById(`${event.target.id}`);
+        if (change < 0) {
+            return
+        }
+        baseTarget.style.display = 'block'
+        baseTarget.style.left = change - window.screen.width + 'px';
+        target.style.display = 'flex';
+        target.style.left = change + 'px';
+    }
+
+    const onUserTouchEnding = (event) => {
+        let change = event.changedTouches[0].clientx - startx;
+        let threshold = window.screen.width / 4;
+        let baseTarget = document.getElementById(`video${event.target.id.substring(4)}`);
+        let target = document.getElementById(`${event.target.id}`);
+        if (change < threshold) {
+            baseTarget.style.left = '-100%';
+            baseTarget.style.display = 'none';
+            target.style.left = 0;
+        } else {
+            baseTarget.style.transition = "all .3s";
+            target.style.transition = "all .3s";
+            baseTarget.style.left = 0;
+            target.style.left = '100%';
+            target.style.display = 'none';
+            baseTarget.style.display = 'block';
+        }
+    }
+
+    const { record : {id,
+        video: { originalUrl },
+        channel},
+        getWidth,getHeight } = props;
+
+    return (
+        <div id="check">
+            <div style={{ clear: 'both' }}>
+                <video width={getWidth} height={getHeight} controls
+                    id={`video${id}`} onTouchStart={(event) => onTouchStarting(event)}
+                    onTouchMove={(event) => onTouchMoving(event)} onTouchEnd={(event => onTouchEnding(event))}>
+                    <source src={originalUrl} type="video/mp4" />
+                </video>
+            </div>
+            <div id={`user${id}`} className="userName"
+                onTouchStart={(event) => onUserTouchStarting(event)}
+                onTouchMove={(event) => onUserTouchMoving(event)}
+                onTouchEnd={(event => onUserTouchEnding(event))}>
+                {channel&& channel.user.name? channel.user.name : `#user`}
+            </div>
+        </div>
+    );
+}
+
+export default VideoDisplay;
---
+++ src/pages/dashboard.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Data } from '../constants/sample.js';
+import VideoDisplay from '../components/videoDisplay.js'
+
+function Dashboard() {
+    const [getHeight, setHeight] = useState(window.screen.height);
+    const [getWidth, setWidth] = useState(window.screen.width);
+    // const [getLastScrollTop, setLastScrollTop] = useState(0);
+    // const [scrolled, setScroll] = useState(false);
+    let scrolled=false;
+    let lastScrolled=window.pageYOffset || document.documentElement.scrollTop;;
+    let scrollHeight=0;
+
+    const setHeightandWidth = () => {
+        setHeight(window.screen.height);
+        setWidth(window.screen.width);
+    }
+
+    const setScrollHeight = async (event) => {
+        console.log(lastScrolled)
+        if (!scrolled) {
+            scrolled=true;
+            let scrollToTop = window.pageYOffset || document.documentElement.scrollTop;
+            let lastScrollTop = 0;
+            if (scrollToTop > lastScrolled) {
+                lastScrollTop = scrollHeight + window.screen.height;
+            } else {
+                lastScrollTop = scrollHeight - window.screen.height;               
+            }
+            scrollHeight=lastScrollTop;
+            window.scrollTo(0, scrollHeight)
+            lastScrolled = scrollToTop <= 0 ? 0 : scrollToTop;
+            setTimeout(() => { if(scrolled) scrolled=false }, 500);
+        }
+
+    }
+
+useEffect(() => {
+    window.addEventListener("resize", setHeightandWidth);
+    window.addEventListener("scroll", (event) => setScrollHeight(event));
+
+    return () => {
+        window.removeEventListener("resize", setHeightandWidth);
+        window.removeEventListener("scroll", () => { });
+    }
+})
+
+
+
+return (
+    <div>
+        {Data.map((record) => {
+            return <VideoDisplay key={record.id} record={record} getWidth={getWidth} getHeight={getHeight} />
+        })}
+    </div>
+)
+}
+
+export default Dashboard;
