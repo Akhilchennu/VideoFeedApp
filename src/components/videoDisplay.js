1bd825f04bd260882c62ae7fd02f47074f38b435
Akhil Chennu <akhilchennu@Akhils-MacBook-Pro.local>
2020-10-11 21:27:48 +0530

    code changes

--- src/components/videoDisplay.js
+++ src/components/videoDisplay.js
@@ -1,108 +1,96 @@
 import React, { useState } from 'react';
 
-function VideoDisplay(props){
+function VideoDisplay(props) {
 
-    const [startx, setStartx] = useState(0);
+    const [startx, setStartx] = useState(null);
+    const [starty, setStarty] = useState(null);
 
     const onTouchStarting = (event) => {
         setStartx(event.touches[0].clientX);
+        setStarty(event.touches[0].clientY);
     }
 
     const onTouchMoving = (event) => {
-        let touch = event.touches[0];
-        let change = startx-touch.clientX;
-        let baseTarget = document.getElementById(`${event.target.id}`);
-        let target = document.getElementById(`user${event.target.id.substring(5)}`)
-        if (change < 0) {
-            return
+        if (startx === null) {
+            return;
         }
-        baseTarget.style.left = '-' + change + 'px';
-        target.style.display = 'block';
-        target.style.left = ((window.screen.width) - change) + 'px';
-    }
 
-    const onTouchEnding = (event) => {
-        let change = startx - event.changedTouches[0].clientX;
-        let threshold = window.screen.width / 3;
+        if (starty === null) {
+            return;
+        }
         let baseTarget = document.getElementById(`${event.target.id}`);
         let target = document.getElementById(`user${event.target.id.substring(5)}`)
-        if (change < threshold) {
-            baseTarget.style.left = 0;
-            target.style.left = '100%';
-            target.style.display = 'none';
-        } else {
-            baseTarget.style.transition = "all .3s";
-            target.style.transition = "all .3s";
-            baseTarget.style.display = '-100%';
-            target.style.left = '0';
-            target.style.display = 'flex';
-            baseTarget.style.display = 'none'
-            target.style.height = `${window.screen.height}px`;
-            target.style.width = `${window.screen.width}px`;
-        }
+        let diffX = startx - event.touches[0].clientX;
+        let diffY = starty - event.touches[0].clientY;
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (diffX > 0) {
+                baseTarget.style.transition = "all .3s";
+                target.style.transition = "all .3s";
+                baseTarget.style.display = '-100%';
+                target.style.left = '0';
+                target.style.display = 'flex';
+                baseTarget.style.display = 'none'
+                target.style.height = `${window.screen.height}px`;
+                target.style.width = `${window.screen.width}px`;
+
+            }
+        } 
+        setStartx(event.touches[0].clientX);
+        setStarty(event.touches[0].clientY);
     }
 
     const onUserTouchStarting = (event) => {
         setStartx(event.touches[0].clientX);
-        let baseTarget = document.getElementById(`video${event.target.id.substring(4)}`);
-        let target = document.getElementById(`${event.target.id}`);
-        baseTarget.style.transition = '';
-        target.style.transition = '';
-        baseTarget.style.display = 'none'
+        setStarty(event.touches[0].clientY);
     }
 
     const onUserTouchMoving = (event) => {
-        let touch = event.touches[0];
-        let change = touch.clientX - startx;
-        let baseTarget = document.getElementById(`video${event.target.id.substring(4)}`);
-        let target = document.getElementById(`${event.target.id}`);
-        if (change < 0) {
-            return
+        if (startx === null) {
+            return;
         }
-        baseTarget.style.display = 'block'
-        baseTarget.style.left = change - window.screen.width + 'px';
-        target.style.display = 'flex';
-        target.style.left = change + 'px';
-    }
 
-    const onUserTouchEnding = (event) => {
-        let change = event.changedTouches[0].clientX - startx;
-        let threshold = window.screen.width / 4;
+        if (starty === null) {
+            return;
+        }
         let baseTarget = document.getElementById(`video${event.target.id.substring(4)}`);
         let target = document.getElementById(`${event.target.id}`);
-        if (change < threshold) {
-            baseTarget.style.left = '-100%';
-            baseTarget.style.display = 'none';
-            target.style.left = 0;
-        } else {
+        let diffX = startx - event.touches[0].clientX;
+        let diffY = starty - event.touches[0].clientY;
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (diffX < 0) {
             baseTarget.style.transition = "all .3s";
             target.style.transition = "all .3s";
             baseTarget.style.left = 0;
             target.style.left = '100%';
             target.style.display = 'none';
             baseTarget.style.display = 'block';
-        }
+
+            }
+        } 
+        setStartx(event.touches[0].clientX);
+        setStarty(event.touches[0].clientY);
     }
 
-    const { record : {id,
+    const { record: { id,
         video: { originalUrl },
-        channel},
-        getWidth,getHeight } = props;
+        channel },
+        getWidth, getHeight } = props;
 
     return (
         <div id="check">
             <div style={{ clear: 'both' }}>
                 <video width={getWidth} height={getHeight} controls
                     id={`video${id}`} onTouchStart={(event) => onTouchStarting(event)}
-                    onTouchMove={(event) => onTouchMoving(event)} onTouchEnd={(event => onTouchEnding(event))}>
+                    onTouchMove={(event) => onTouchMoving(event)} >
                     <source src={originalUrl} type="video/mp4" />
                 </video>
             </div>
             <div id={`user${id}`} className="userName"
                 onTouchStart={(event) => onUserTouchStarting(event)}
-                onTouchMove={(event) => onUserTouchMoving(event)}
-                onTouchEnd={(event => onUserTouchEnding(event))}>
-                {channel&& channel.user.name? channel.user.name : `#user`}
+                onTouchMove={(event) => onUserTouchMoving(event)}>
+                {channel && channel.user.name ? channel.user.name : `#user`}
             </div>
         </div>
     );
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,35 +5,12 @@ import VideoDisplay from '../components/videoDisplay.js'
 function Dashboard() {
     const [getHeight, setHeight] = useState(window.screen.height);
     const [getWidth, setWidth] = useState(window.screen.width);
-    // const [getLastScrollTop, setLastScrollTop] = useState(0);
-    // const [scrolled, setScroll] = useState(false);
     let scrolled=false;
-    // let lastScrolled=window.pageYOffset || document.documentElement.scrollTop;;
-    // let scrollHeight=0;
 
     const setHeightandWidth = () => {
         setHeight(window.screen.height);
         setWidth(window.screen.width);
     }
-
-    // const setScrollHeight = async (event) => {
-    //     event.preventDefault();
-    //     if (!scrolled) {
-    //         scrolled=true;
-    //         let scrollToTop = window.pageYOffset || document.documentElement.scrollTop;
-    //         let lastScrollTop = 0;
-    //         if (scrollToTop > lastScrolled) {
-    //             lastScrollTop = scrollHeight + window.screen.height;
-    //         } else {
-    //             lastScrollTop = scrollHeight - window.screen.height;               
-    //         }
-    //         scrollHeight=lastScrollTop;
-    //         window.scrollTo(0, scrollHeight)
-    //         lastScrolled = scrollToTop <= 0 ? 0 : scrollToTop;
-    //         setTimeout(() => { if(scrolled) scrolled=false }, 500);
-    //         event.stopPropagation();
-    //     }
-    // }
 
     let lastScrolled=(document.body.getBoundingClientRect()).top;
     let scrollHeight=0;
